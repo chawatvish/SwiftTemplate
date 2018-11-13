9b3e46214faa484404552feca619a0c30d694750
chawatvish <chawatvish.wo@gmail.com>
2018-11-13 10:23:21 +0700

    Modified format

--- Swift File.xctemplate/___FILEBASENAME___.swift
+++ Swift File.xctemplate/___FILEBASENAME___.swift
@@ -3,23 +3,35 @@
 import UIKit
 
 class ___FILEBASENAME___ {
-
+    
     //MARK: -
     //MARK: UI
-
+    
     //MARK: -
     //MARK: Properties
     
     //MARK: -
     //MARK: Init
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    static func getInstance(text:String) -> ___FILEBASENAME___ {
+        let viewController = ___FILEBASENAME___(nibName: "Nib NAME", bundle: nil)
+        return viewController
+    }
     
     //MARK: -
     //MARK: ViewController
     override func viewDidLoad() {
-        super.viewDidLoad()    
+        super.viewDidLoad()
         
     }
-
+    
     //MARK: -
     //MARK: Action
     
