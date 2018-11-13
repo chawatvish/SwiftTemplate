5799279a1d513a24e69775c89e47c9657d880c3e
chawatvish <chawatvish.wo@gmail.com>
2018-11-13 10:13:19 +0700

    init project

---
+++ Swift File.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___ {
+
+    //MARK: -
+    //MARK: UI
+
+    //MARK: -
+    //MARK: Properties
+    
+    //MARK: -
+    //MARK: Init
+    
+    //MARK: -
+    //MARK: ViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()    
+        
+    }
+
+    //MARK: -
+    //MARK: Action
+    
+    //MARK: -
+    //MARK: Private Method
+}
